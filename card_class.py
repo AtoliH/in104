cb69b4bd7ae59466c1d5c5c3b269d770d71b5322
Fire <atoli.huppe@gmail.com>
2020-04-10 12:10:48 +0200

    Ajouter membre points

--- card_class.py
+++ card_class.py
@@ -1,11 +1,11 @@
 """
 Module card2.
 Definit une classe Card et un petit cas-test qui demontre son utilite.
-TODO: 
+TODO:
   - coder la fonction str
   - le init qui prend 0 ou 2 parametres (indication : chercher sur StackOverflow "python optional arguments in initializer of python class")
-  - coder points ; d'ailleurs, est-ce une fonction ou un attribut ? 
-  - corriger le bug : qui est que le 10 est plus fort que Dame Roi Valet (7 8 9 V D R 10 As) 
+  - coder points ; d'ailleurs, est-ce une fonction ou un attribut ?
+  - corriger le bug : qui est que le 10 est plus fort que Dame Roi Valet (7 8 9 V D R 10 As)
   - bonus : ajouter les atouts
   - bonus : ajouter un setter qui empeche couleur = "VERTE"
 """
@@ -13,20 +13,20 @@ from enum import Enum
 
 
 Color = Enum("Color", "COEUR CARREAU PIQUE TREFLE")
-	
+
 Valeur = Enum("Valeur", "SEPT HUIT NEUF VALET DAME ROI DIX AS")
 
 class Card:
 	def __init__(self, valeur = None, couleur = None):
 		self.couleur = couleur
 		self.valeur = valeur
-
+		self.points = 0
 
 	def __str__(self):
 		p = str(Color(self.couleur).name)
 		q = str(Valeur(self.valeur).name)
 		return (q + " de " + p)
-        
+
 	def points_carte (self):
 		if (self.valeur == Valeur.SEPT) or (self.valeur == Valeur.HUIT) or (self.valeur == Valeur.NEUF):
 			self.points = 0
@@ -44,12 +44,12 @@ class Card:
 		return self.valeur.value
 card1 = Card(Valeur.ROI, Color.PIQUE)
 card2 = Card(Valeur.DIX, Color.PIQUE)
-print("Le joueur 1 joue la carte", card1) 
+print("Le joueur 1 joue la carte", card1)
 print("Le joueur 2 joue la carte", card2)
 print("qui a la meilleure carte ?")
 
 ## maintenant on compare des cartes
-# atout ? 
+# atout ?
 p1 = card1.points
 p2 = card2.points
 
